4fd12bad267c3fe19fd9694409028ee784f944b1
mollynordh <molly@hejalbert.se>
2017-11-17 09:38:06 +0100

    something

---
+++ data/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_spritesheet" tilewidth="70" tileheight="70" tilecount="169" columns="13">
+ <image source="../images/tiles_spritesheet.png" width="914" height="936"/>
+</tileset>
