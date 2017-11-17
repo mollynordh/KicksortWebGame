74bc7dc91526754b58984bea3f981d63685f8226
mollynordh <molly@hejalbert.se>
2017-11-17 16:51:17 +0100

    new graphics

---
+++ data/untitledd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kit" tilewidth="70" tileheight="70" tilecount="12" columns="6">
+ <image source="../../../Downloads/kit.png" width="420" height="140"/>
+</tileset>
